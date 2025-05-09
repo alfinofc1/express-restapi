10e0c5653e8bd6874055a185386a4082cf16f97d
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-05-09 10:41:19 +0700

    Add files via upload

--- routes/route.js
+++ routes/route.js
@@ -461,6 +461,30 @@ router.get('/mlbb', async (req, res) => {
     }
 });
 
+router.get('/random', async (req, res) => {
+    try {
+        const text = req.query.text;
+        if (!text) return res.status(400).json({ creator: "ALFIN", result: false, message: "Harap masukkan parameter prompt!" });
+
+        const response = await axios.get(`https://toxicdevilapi.vercel.app/random/anime?type=sfw`, {
+            responseType: 'arraybuffer' // Penting: minta respons sebagai arraybuffer
+        });
+
+        const imageBuffer = Buffer.from(response.data, 'binary'); // Convert data to Buffer
+
+        // Tetapkan Content-Type berdasarkan jenis gambar (sesuaikan jika perlu)
+        res.setHeader('Content-Type', 'image/jpeg'); // Asumsi: gambar adalah JPEG
+        // Opsi lain: 'image/png', 'image/gif', dll. Tergantung jenis gambar yang dikembalikan API.
+
+        res.send(imageBuffer); // Kirim data gambar sebagai respons
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ creator: "ALFIN", result: false, message: "Gagal mendapatkan gambar dari .", error: error.message });
+    } finally {
+        console.log('Gambar dari  request completed.');
+    }
+});
+
 router.get('/editor', async (req, res) => {
     try {
         const text = req.query.text;
